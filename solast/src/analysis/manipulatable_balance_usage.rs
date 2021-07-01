df1b1c28c540f66bf43e6a77850b14f62a1dba20
Camden Smallwood <camden.smallwood@gmail.com>
2021-07-01 00:46:52 -0400

    Some cleanup

--- solast/src/analysis/manipulatable_balance_usage.rs
+++ solast/src/analysis/manipulatable_balance_usage.rs
@@ -1,4 +1,5 @@
 use super::AstVisitor;
+use solidity::ast::*;
 
 pub struct ManipulatableBalanceUsageVisitor;
 
@@ -10,86 +11,70 @@ pub struct ManipulatableBalanceUsageVisitor;
 impl AstVisitor for ManipulatableBalanceUsageVisitor {
     fn visit_function_call<'a>(
         &mut self,
-        _source_unit: &'a solidity::ast::SourceUnit,
-        contract_definition: &'a solidity::ast::ContractDefinition,
-        definition_node: &'a solidity::ast::ContractDefinitionNode,
-        _blocks: &mut Vec<&'a solidity::ast::Block>,
-        _statement: Option<&'a solidity::ast::Statement>,
-        function_call: &'a solidity::ast::FunctionCall,
+        _source_unit: &'a SourceUnit,
+        contract_definition: &'a ContractDefinition,
+        definition_node: &'a ContractDefinitionNode,
+        _blocks: &mut Vec<&'a Block>,
+        _statement: Option<&'a Statement>,
+        function_call: &'a FunctionCall,
     ) -> std::io::Result<()> {
+        if function_call.arguments.len() != 1 {
+            return Ok(())
+        }
 
         match function_call.expression.as_ref() {
-            solidity::ast::Expression::MemberAccess(solidity::ast::MemberAccess {
+            Expression::MemberAccess(MemberAccess {
                 member_name,
                 ..
-            }) if member_name == "balanceOf" => {}
-
-            _ => return Ok(()),
-        }
-
-        if function_call.arguments.len() != 1 {
-            return Ok(());
-        }
-
-        match function_call.arguments.first().unwrap() {
-            solidity::ast::Expression::FunctionCall(solidity::ast::FunctionCall {
-                expression,
-                arguments,
-                ..
-            }) => match expression.as_ref() {
-                solidity::ast::Expression::ElementaryTypeNameExpression(
-                    solidity::ast::ElementaryTypeNameExpression {
-                        type_name:
-                            solidity::ast::TypeName::ElementaryTypeName(
-                                solidity::ast::ElementaryTypeName { name, .. },
-                            ),
+            }) if member_name == "balanceOf" => match function_call.arguments.first().unwrap() {
+                Expression::FunctionCall(FunctionCall {
+                    expression,
+                    arguments,
+                    ..
+                }) => match expression.as_ref() {
+                    Expression::ElementaryTypeNameExpression(ElementaryTypeNameExpression {
+                        type_name: TypeName::ElementaryTypeName(ElementaryTypeName { name, .. }),
                         ..
-                    },
-                ) if name == "address" && arguments.len() == 1 => {
-                    match arguments.first().unwrap() {
-                        solidity::ast::Expression::Identifier(solidity::ast::Identifier {
+                    }) if name == "address" && arguments.len() == 1 => match arguments.first().unwrap() {
+                        Expression::Identifier(Identifier {
                             name,
                             ..
-                        }) if name == "this" => {
-                            //
-                            // TODO: mark usage of `.balanceOf(address(this))` for future checks
-                            //
+                        }) if name == "this" => match definition_node {
+                            ContractDefinitionNode::FunctionDefinition(function_definition) => println!(
+                                "\tThe {} {} in the `{}` {} contains usage of `.balanceOf(address(this))`",
 
-                            match definition_node {
-                                solidity::ast::ContractDefinitionNode::FunctionDefinition(function_definition) => println!(
-                                    "\tThe {} {} in the `{}` {} contains usage of `.balanceOf(address(this))`",
+                                function_definition.visibility,
 
-                                    function_definition.visibility,
+                                if let FunctionKind::Constructor = function_definition.kind {
+                                    format!("{}", "constructor")
+                                } else {
+                                    format!("`{}` {}", function_definition.name, function_definition.kind)
+                                },
 
-                                    if let solidity::ast::FunctionKind::Constructor = function_definition.kind {
-                                        format!("{}", "constructor")
-                                    } else {
-                                        format!("`{}` {}", function_definition.name, function_definition.kind)
-                                    },
+                                contract_definition.name,
+                                contract_definition.kind
+                            ),
 
-                                    contract_definition.name,
-                                    contract_definition.kind,
-                                ),
+                            ContractDefinitionNode::ModifierDefinition(modifier_definition) => println!(
+                                "\tThe `{}` modifier in the `{}` {} contains usage of `.balanceOf(address(this))`",
+                                modifier_definition.name,
+                                contract_definition.name,
+                                contract_definition.kind
+                            ),
 
-                                solidity::ast::ContractDefinitionNode::ModifierDefinition(modifier_definition) => println!(
-                                    "\tThe `{}` modifier in the `{}` {} contains usage of `.balanceOf(address(this))`",
-                                    modifier_definition.name,
-                                    contract_definition.name,
-                                    contract_definition.kind,
-                                ),
-
-                                _ => return Ok(())
-                            }
+                            _ => return Ok(())
                         }
 
-                        _ => return Ok(()),
+                        _ => return Ok(())
                     }
+    
+                    _ => return Ok(())
                 }
+    
+                _ => return Ok(())
+            }
 
-                _ => return Ok(()),
-            },
-
-            _ => return Ok(()),
+            _ => return Ok(())
         }
 
         Ok(())
