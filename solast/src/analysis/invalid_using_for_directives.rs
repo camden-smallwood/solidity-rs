f656d2618d5b5f476a5e3b35aaa2c6d29c8e6243
Camden Smallwood <camden.smallwood@gmail.com>
2021-07-06 22:33:16 -0400

    Some cleanup to 'invalid_using_for_directives' visitor

--- solast/src/analysis/invalid_using_for_directives.rs
+++ solast/src/analysis/invalid_using_for_directives.rs
@@ -1,16 +1,30 @@
+use super::{AstVisitor, UsingForDirectiveContext};
+
 pub struct InvalidUsingForDirectivesVisitor;
 
-impl super::AstVisitor for InvalidUsingForDirectivesVisitor {
-    fn visit_using_for_directive<'a>(&mut self, context: &mut super::UsingForDirectiveContext<'a>) -> std::io::Result<()> {
+impl AstVisitor for InvalidUsingForDirectivesVisitor {
+    fn visit_using_for_directive<'a>(&mut self, context: &mut UsingForDirectiveContext<'a>) -> std::io::Result<()> {
+        //
+        // Get the identifier of the used library's contract definition
+        //
+
         let using_contract_id = match context.using_for_directive.library_name.referenced_declaration.as_ref() {
             Some(&id) => id,
             None => return Ok(())
         };
 
+        //
+        // Get the type name of the requested type to use the library for
+        //
+
         let for_type_name = match context.using_for_directive.type_name.as_ref() {
             Some(type_name) => type_name,
             None => return Ok(())
         };
+
+        //
+        // Attempt to retrieve the contract definition associated with the used library
+        //
 
         let mut using_contract_definition = None;
 
@@ -21,30 +35,42 @@ impl super::AstVisitor for InvalidUsingForDirectivesVisitor {
             }
         }
 
-        if let Some(using_contract_definition) = using_contract_definition {
-            let mut function_found = false;
+        if using_contract_definition.is_none() {
+            return Ok(())
+        }
 
-            for function_definition in using_contract_definition.function_definitions() {
-                if let Some(parameter) = function_definition.parameters.parameters.first() {
-                    if let Some(type_name) = parameter.type_name.as_ref() {
-                        if type_name == for_type_name {
-                            function_found = true;
-                            break;
-                        }
+        let using_contract_definition = using_contract_definition.unwrap();
+
+        //
+        // Determine if the library contains a usable function for the requested type
+        //
+
+        let mut usable_function_found = false;
+
+        for function_definition in using_contract_definition.function_definitions() {
+            if let Some(parameter) = function_definition.parameters.parameters.first() {
+                if let Some(type_name) = parameter.type_name.as_ref() {
+                    if type_name == for_type_name {
+                        usable_function_found = true;
+                        break;
                     }
                 }
             }
+        }
 
-            if !function_found {
-                println!(
-                    "\tThe `{}` {} contains an invalid using-for directive: `{}`",
+        //
+        // If the library does not contain any usable functions for the requested type, print a message
+        //
 
-                    context.contract_definition.name,
-                    context.contract_definition.kind,
+        if !usable_function_found {
+            println!(
+                "\tThe `{}` {} contains an invalid using-for directive: `{}`",
 
-                    context.using_for_directive
-                );
-            }
+                context.contract_definition.name,
+                context.contract_definition.kind,
+
+                context.using_for_directive
+            );
         }
 
         Ok(())
