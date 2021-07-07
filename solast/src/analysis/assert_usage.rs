af5b6873eebcd3a6f050d46bcb06c4ebd017f184
Camden Smallwood <camden.smallwood@gmail.com>
2021-07-06 22:58:05 -0400

    Some cleanup to 'assert_usage' visitor

--- solast/src/analysis/assert_usage.rs
+++ solast/src/analysis/assert_usage.rs
@@ -1,6 +1,6 @@
-use super::AstVisitor;
+use super::{AstVisitor, FunctionCallContext};
 use solidity::ast::*;
-use std::{collections::HashSet, io};
+use std::collections::HashSet;
 
 pub struct AssertUsageVisitor {
     reported_definitions: HashSet<NodeID>,
@@ -15,45 +15,72 @@ impl Default for AssertUsageVisitor {
 }
 
 impl AstVisitor for AssertUsageVisitor {
-    fn visit_function_call<'a, 'b>(&mut self, context: &mut super::FunctionCallContext<'a, 'b>) -> io::Result<()> {
+    fn visit_function_call<'a, 'b>(&mut self, context: &mut FunctionCallContext<'a, 'b>) -> std::io::Result<()> {
+        //
+        // Get the identifier associated with the function or modifier containing the function call
+        //
+
         let definition_id = match context.definition_node {
-            &ContractDefinitionNode::FunctionDefinition(FunctionDefinition { id, .. })
-            | &ContractDefinitionNode::ModifierDefinition(ModifierDefinition { id, .. }) => id,
+            ContractDefinitionNode::FunctionDefinition(FunctionDefinition { id, .. }) |
+            ContractDefinitionNode::ModifierDefinition(ModifierDefinition { id, .. }) => id.clone(),
 
             _ => return Ok(())
         };
 
-        if let Expression::Identifier(Identifier { name, .. }) = context.function_call.expression.as_ref() {
-            if name == "assert" && !self.reported_definitions.contains(&definition_id) {
-                self.reported_definitions.insert(definition_id);
+        //
+        // Check if the expression of function call is the "assert" identifier
+        //
 
-                match context.definition_node {
-                    ContractDefinitionNode::FunctionDefinition(function_definition) => println!(
-                        "\tThe {} {} in the `{}` {} contains `assert` usage",
+        let is_assert = match context.function_call.expression.as_ref() {
+            Expression::Identifier(Identifier { name, .. }) => name == "assert",
+            _ => false
+        };
 
-                        function_definition.visibility,
+        if !is_assert {
+            return Ok(())
+        }
 
-                        if let FunctionKind::Constructor = function_definition.kind {
-                            format!("{}", "constructor")
-                        } else {
-                            format!("`{}` {}", function_definition.name, function_definition.kind)
-                        },
+        //
+        // Don't display multiple messages for the same function or modifier
+        //
 
-                        context.contract_definition.name,
-                        context.contract_definition.kind,
-                    ),
+        if self.reported_definitions.contains(&definition_id) {
+            return Ok(())
+        }
 
-                    ContractDefinitionNode::ModifierDefinition(modifier_definition) => println!(
-                        "\tThe {} `{}` modifier in the `{}` {} contains `assert` usage",
-                        modifier_definition.visibility,
-                        modifier_definition.name,
-                        context.contract_definition.name,
-                        context.contract_definition.kind,
-                    ),
+        self.reported_definitions.insert(definition_id);
 
-                    _ => ()
-                }
-            }
+        //
+        // Print a message about the assert usage
+        //
+
+        match context.definition_node {
+            ContractDefinitionNode::FunctionDefinition(function_definition) => println!(
+                "\tThe {} {} in the `{}` {} contains `assert` usage",
+
+                function_definition.visibility,
+
+                if let FunctionKind::Constructor = function_definition.kind {
+                    format!("{}", "constructor")
+                } else {
+                    format!("`{}` {}", function_definition.name, function_definition.kind)
+                },
+
+                context.contract_definition.name,
+                context.contract_definition.kind
+            ),
+
+            ContractDefinitionNode::ModifierDefinition(modifier_definition) => println!(
+                "\tThe {} `{}` modifier in the `{}` {} contains `assert` usage",
+
+                modifier_definition.visibility,
+                modifier_definition.name,
+
+                context.contract_definition.name,
+                context.contract_definition.kind
+            ),
+
+            _ => {}
         }
         
         Ok(())
