d213670fba9aaf664ff6ad9bfe544bf8a4e2feb9
Camden Smallwood <camden.smallwood@gmail.com>
2021-07-06 22:48:48 -0400

    Some cleanup to 'abstract_contracts' visitor

--- solast/src/analysis/abstract_contracts.rs
+++ solast/src/analysis/abstract_contracts.rs
@@ -1,39 +1,37 @@
 use super::{AstVisitor, FunctionDefinitionContext};
 use solidity::ast::{FunctionKind, Visibility};
-use std::io;
 
 pub struct AbstractContractsVisitor;
 
 impl AstVisitor for AbstractContractsVisitor {
-    fn visit_function_definition<'a>(&mut self, context: &mut FunctionDefinitionContext<'a>) -> io::Result<()> {
+    fn visit_function_definition<'a>(&mut self, context: &mut FunctionDefinitionContext<'a>) -> std::io::Result<()> {
+        //
+        // Only check function definitions associated with constructors
+        //
+
         if context.function_definition.kind != FunctionKind::Constructor {
             return Ok(())
         }
+
+        //
+        // Only check function definitions with internal visibility
+        //
 
         if context.function_definition.visibility != Visibility::Internal {
             return Ok(())
         }
 
-        match context.contract_definition.is_abstract {
-            None | Some(false) => {
-                println!(
-                    "\tThe constructor of the `{}` contract is marked {} instead of marking `{}` as abstract",
-                    context.contract_definition.name,
-                    context.function_definition.visibility,
-                    context.contract_definition.name,
-                );
-            }
+        //
+        // If the constructor is marked internal and the contract is not abstract, print a message
+        //
 
-            Some(true) => {
-                if context.function_definition.visibility == Visibility::Internal {
-                    println!(
-                        "\tThe constructor of the `{}` contract is marked {} when `{}` is already marked as abstract",
-                        context.contract_definition.name,
-                        context.function_definition.visibility,
-                        context.contract_definition.name,
-                    );
-                }
-            }
+        if let None | Some(false) = context.contract_definition.is_abstract {
+            println!(
+                "\tThe constructor of the `{}` contract is marked {} instead of marking `{}` as abstract",
+                context.contract_definition.name,
+                context.function_definition.visibility,
+                context.contract_definition.name,
+            );
         }
 
         Ok(())
