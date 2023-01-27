2b61f2f102301b91c8f247fdfa294e24de22d61f
Camden Smallwood <camden.smallwood@gmail.com>
2023-01-27 15:08:13 -0500

    Clean up `unrestricted_setter_functions` analyzer

--- solast/src/analysis/unrestricted_setter_functions.rs
+++ solast/src/analysis/unrestricted_setter_functions.rs
@@ -3,6 +3,20 @@ use std::io;
 
 pub struct UnrestrictedSetterFunctionsVisitor;
 
+impl UnrestrictedSetterFunctionsVisitor {
+    fn print_message(
+        &mut self,
+        contract_definition: &ContractDefinition,
+        definition_node: &ContractDefinitionNode,
+        source_line: usize,
+    ) {
+        println!(
+            "\t{} is an unprotected setter function",
+            contract_definition.definition_node_location(source_line, definition_node),
+        );
+    }
+}
+
 impl AstVisitor for UnrestrictedSetterFunctionsVisitor {
     fn visit_function_definition<'a>(&mut self, context: &mut FunctionDefinitionContext<'a>) -> io::Result<()> {
         if let FunctionKind::Constructor = context.function_definition.kind {
@@ -40,22 +54,10 @@ impl AstVisitor for UnrestrictedSetterFunctionsVisitor {
             }
         }
 
-        println!(
-            "\tL{}: {:?} {} {} ({}) is an unprotected setter function",
-
+        self.print_message(
+            context.contract_definition,
+            context.definition_node,
             context.current_source_unit.source_line(context.function_definition.src.as_str())?,
-
-            context.function_definition.visibility,
-
-            if let FunctionKind::Constructor = context.function_definition.kind {
-                context.contract_definition.name.to_string()
-            } else {
-                format!("{}.{}", context.contract_definition.name, context.function_definition.name)
-            },
-
-            context.function_definition.kind,
-
-            context.function_definition.src
         );
 
         Ok(())
