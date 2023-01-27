a7349bd2fd5c20512c85a98cca811390df258801
Camden Smallwood <camden.smallwood@gmail.com>
2023-01-27 15:22:32 -0500

    Clean up the `unused_return` analyzer

--- solast/src/analysis/unused_return.rs
+++ solast/src/analysis/unused_return.rs
@@ -2,6 +2,24 @@ use solidity::ast::*;
 
 pub struct UnusedReturnVisitor;
 
+impl UnusedReturnVisitor {
+    fn print_message(
+        &mut self,
+        contract_definition: &ContractDefinition,
+        definition_node: &ContractDefinitionNode,
+        source_line: usize,
+        called_name: &str,
+        return_parameter_count: usize,
+    ) {
+        println!(
+            "\t{} makes a call to the {}, ignoring the returned {}",
+            contract_definition.definition_node_location(source_line, definition_node),
+            called_name,
+            if return_parameter_count == 1 { "value" } else { "values" },
+        );
+    }
+}
+
 impl AstVisitor for UnusedReturnVisitor {
     fn visit_statement<'a, 'b>(&mut self, context: &mut StatementContext<'a, 'b>) -> std::io::Result<()> {
         let (referenced_declaration, src) = match context.statement {
@@ -31,77 +49,31 @@ impl AstVisitor for UnusedReturnVisitor {
 
         for source_unit in context.source_units.iter() {
             if let Some((called_contract_definition, called_function_definition)) = source_unit.function_and_contract_definition(referenced_declaration) {
-                if called_function_definition.return_parameters.parameters.is_empty() {
-                    return Ok(())
+                if !called_function_definition.return_parameters.parameters.is_empty() {
+                    self.print_message(
+                        context.contract_definition,
+                        context.definition_node,
+                        context.current_source_unit.source_line(src)?,
+
+                        format!(
+                            "{} `{}` {}",
+
+                            format!("{:?}", called_function_definition.visibility).to_lowercase(),
+    
+                            if called_function_definition.name.is_empty() {
+                                called_contract_definition.name.to_string()
+                            } else {
+                                format!("{}.{}", called_contract_definition.name, called_function_definition.name)
+                            },
+    
+                            format!("{:?}", called_function_definition.kind).to_lowercase(),
+                        ).as_str(),
+
+                        called_function_definition.return_parameters.parameters.len(),
+                    );
                 }
                 
-                match context.definition_node {
-                    ContractDefinitionNode::FunctionDefinition(function_definition) => println!(
-                        "\tL{}: The {} `{}` {} makes a call to the {} `{}` {}, ignoring the returned {}",
-                        
-                        context.current_source_unit.source_line(src)?,
-
-                        function_definition.visibility,
-
-                        if function_definition.name.is_empty() {
-                            context.contract_definition.name.to_string()
-                        } else {
-                            format!("{}.{}", context.contract_definition.name, function_definition.name)
-                        },
-
-                        function_definition.kind,
-
-                        format!("{:?}", called_function_definition.visibility).to_lowercase(),
-
-                        if called_function_definition.name.is_empty() {
-                            called_contract_definition.name.to_string()
-                        } else {
-                            format!("{}.{}", called_contract_definition.name, called_function_definition.name)
-                        },
-
-                        format!("{:?}", called_function_definition.kind).to_lowercase(),
-
-                        if called_function_definition.return_parameters.parameters.len() > 1 {
-                            "values"
-                        } else {
-                            "value"
-                        }
-                    ),
-
-                    ContractDefinitionNode::ModifierDefinition(modifier_definition) => println!(
-                        "\tL{}: The {} `{}` modifier makes a call to the {} `{}` {}, ignoring the returned {}",
-
-                        context.current_source_unit.source_line(src)?,
-
-                        format!("{:?}", modifier_definition.visibility).to_lowercase(),
-
-                        if modifier_definition.name.is_empty() {
-                            context.contract_definition.name.to_string()
-                        } else {
-                            format!("{}.{}", context.contract_definition.name, modifier_definition.name)
-                        },
-
-                        format!("{:?}", called_function_definition.visibility).to_lowercase(),
-
-                        if called_function_definition.name.is_empty() {
-                            called_contract_definition.name.to_string()
-                        } else {
-                            format!("{}.{}", called_contract_definition.name, called_function_definition.name)
-                        },
-
-                        format!("{:?}", called_function_definition.kind).to_lowercase(),
-
-                        if called_function_definition.return_parameters.parameters.len() > 1 {
-                            "values"
-                        } else {
-                            "value"
-                        }
-                    ),
-
-                    _ => ()
-                }
-
-                return Ok(())
+                break;
             }
         }
 
